0c535946225d806cebce922eada96ae7ea003641
Pieter Hagen <phagen@splunk.com>
2024-02-20 17:23:34 +0100

    patch script added

---
+++ dist/kubernetes/scripts/pods-instrument-on.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+kubectl patch deployment ui -p '{"spec": {"template":{"metadata":{"annotations":{"instrumentation.opentelemetry.io/inject-java":"default/splunk-otel-collector"}}}} }'
+kubectl patch deployment orders -p '{"spec": {"template":{"metadata":{"annotations":{"instrumentation.opentelemetry.io/inject-java":"default/splunk-otel-collector"}}}} }'
+kubectl patch deployment carts -p '{"spec": {"template":{"metadata":{"annotations":{"instrumentation.opentelemetry.io/inject-java":"default/splunk-otel-collector"}}}} }'
+kubectl patch deployment catalog -p '{"spec": {"template":{"metadata":{"annotations":{"instrumentation.opentelemetry.io/otel-go-auto-target-exe":"/app/main"}}}} }'
+kubectl patch deployment catalog -p '{"spec": {"template":{"metadata":{"annotations":{"instrumentation.opentelemetry.io/inject-go":"true"}}}} }'
+kubectl patch deployment checkout -p '{"spec": {"template":{"metadata":{"annotations":{"instrumentation.opentelemetry.io/inject-nodejs":"default/splunk-otel-collector"}}}} }'
+kubectl patch deployment assets -p '{"spec": {"template":{"metadata":{"annotations":{"instrumentation.opentelemetry.io/inject-nginx":"false"}}}} }'
